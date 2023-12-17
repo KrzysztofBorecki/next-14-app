9f0efba1298795898d67e114faf1dcfeeb09f9ad
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2023-12-17 20:05:49 +0100

    Add supabase database types

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,9 +1,10 @@
 import { cookies } from 'next/headers';
 import { createServerClient } from '@supabase/ssr';
 import type { CookieOptions } from '@supabase/ssr';
+import type { Database } from '@/lib/supabase/database.types';
 
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
